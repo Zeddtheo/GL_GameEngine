993e1f2bea9782cad3fefdaabc96a6b1fb3540b7
arnaudlabourel <arnaud.labourel@univ-amu.fr>
2022-10-14 09:12:18 +0200

    Corrected files in order to pass checkstyle for tests

--- src/test/java/TestMyClass.java
+++ src/test/java/TestMyClass.java
@@ -1,10 +1,14 @@
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+
+/**
+ * A template class for testing with assertJ.
+ */
 public class TestMyClass {
 
   @Test
-  void testTrue(){
+  void testTrue() {
     assertThat(true).isTrue();
   }
 
