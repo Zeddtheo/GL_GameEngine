667b5a27405695f4f4a3f05bc60a1c5a897b1b8d
MARTINET Theo <theo.martinet@etu.univ-amu.fr>
2022-10-21 14:15:52 +0200

    creation des classe pour game loop

---
+++ src/main/java/game/GameLoop.java
@@ -0,0 +1,9 @@
+package game;
+
+public class GameLoop {
+    public void execute(){
+        while (true){
+            //faire le déroulement du jeu ici qui boucle à l'infinie
+        }
+    }
+}
